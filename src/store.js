68dcf0dde4b07d4afd8a038c8e7288600f8d82cb
FedericoFinco <doppiaeffe94@gmail.com>
2023-09-12 18:49:58 +0200

    added into store better descriptions

--- src/store.js
+++ src/store.js
@@ -10,16 +10,16 @@ export const store = reactive({
 
     events:[
         {
-            title:"Musica Per Compleanno",
-            text:"La musica a una festa di compleanno o a un anniversario è un elemento chiave,Ma come trovare i musicisti più adatti? Su BoolMusic basta selezionare il Genere per visualizzare tutti i musicisti.",
+            title:"Festeggia il Compleanno!",
+            text:"In una festa di compleanno o a un anniversario l'elemento chiave è la Musica,Ma come trovare i musicisti più adatti? Su BoolMusic basta selezionare il Genere per visualizzare tutti i musicisti.",
         },
         {
-            title:"Musica Per Matrimonio",
+            title:"Allieta il Matrimonio",
             text:"La musica a un matrimonio, sia durante il ricevimento che come sottofondo della celebrazione in chiesa o civile, ha un ruolo importantissimo e può rendere questo giorno ancora più speciale.",
         },
         {
-            title:"Musica Per Eventi Privati",
-            text:"La musica a una festa può far divertire, cantare o ballare. Per Questo il giusto dj, cantante o band dal vivo potrà renderla ancora più divertente e memorabile.",
+            title:"Rendi unici gli Eventi Privati",
+            text:"Come vuoi distinguere il tuo evento privato? come vuoi attirare i tuoi ospiti? farli divertire, cantare o ballare?. Per Questo il giusto dj, cantante o band dal vivo potrà renderla ancora più divertente e memorabile.",
         },
     ],
 
@@ -27,7 +27,7 @@ export const store = reactive({
         {
             logo:"../assets/img/logo_step1.png",
             title:"STEP 1",
-            text:"Usa i nostri filtri di ricerca per trovare facilmente la musica giusta tra migliaia di proposte",
+            text:"Naviga tra i musicisti di tutta italia che si mettono a tua disposizione con il loro talento",
         },
         {
             logo:"../assets/img/logo_step2.png",
@@ -36,8 +36,8 @@ export const store = reactive({
         },
         {
             logo:"../assets/img/logo_step3.png",
-            title:"STEP 2",
-            text:"Usa i nostri filtri di ricerca per trovare facilmente la musica giusta tra migliaia di proposte",
+            title:"STEP 3",
+            text:"Contatta le band o i musicisti che ti interessano, cosi da capire quale fà al tuo caso!",
         },
     ]
 
